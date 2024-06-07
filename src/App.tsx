7828601bd91ffbf85495bdb0128f92a4031ff970
Joseph <jvp.2703@gmail.com>
2024-06-07 04:08:25 +0100

    style: snap center dot testing

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,58 @@ function App() {
   const [sizes, setSizes] = useState([50, 50]);
   const snapThresHold = 5;
 
-  return (
-    <main className="grid h-screen grid-rows-[auto,1fr] px-5 pb-5">
-      <header className="text-xl font-bold capitalize leading-[4rem]">readme-jv</header>
-      <article className="grid grid-cols-[auto,1fr] gap-3">
-        <aside className="flex flex-col gap-2 rounded-md border border-stone-100/20 p-3">
-          <Sections />
-        </aside>
+  const dotSnapCss = `
+  .split-panel-snapping {
+    position: relative;
+  }
 
-        <section>
-          <Split
-            className="flex h-full"
-            cursor="col-resize"
-            direction="horizontal"
-            expandToMin={true}
-            gutter={(_, direction) => createGutterElement(direction)}
-            gutterAlign="center"
-            gutterSize={10}
-            minSize={100}
-            sizes={sizes}
-            onDrag={(sizes) => setSizes(sizes)}
-            onDragEnd={(sizes) => handleSnapCenter({sizes, snapThresHold, setSizes})}
-          >
-            <Monaco />
-            <Markdown />
-          </Split>
-        </section>
-      </article>
-    </main>
+  .split-panel-snapping-dots::after {
+    content: '';
+    position: absolute;
+    top: -10px;
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background: #404040;
+    transform: translateX(-50%);
+  }
+
+  .split-panel-snapping-dots::after {
+    left: 50%;
+  }`;
+
+  return (
+    <>
+      <main className="grid h-screen grid-rows-[auto,1fr] px-5 pb-5">
+        <header className="text-xl font-bold capitalize leading-[4rem]">readme-jv</header>
+        <article className="grid grid-cols-[auto,1fr] gap-3">
+          <aside className="flex flex-col gap-2 rounded-md border border-stone-100/20 p-3">
+            <Sections />
+          </aside>
+
+          <section className="split-panel-snapping">
+            <Split
+              className="flex h-full"
+              cursor="col-resize"
+              direction="horizontal"
+              expandToMin={true}
+              gutter={(_, direction) => createGutterElement(direction)}
+              gutterAlign="center"
+              gutterSize={12}
+              minSize={100}
+              sizes={sizes}
+              onDrag={(sizes) => setSizes(sizes)}
+              onDragEnd={(sizes) => handleSnapCenter({sizes, snapThresHold, setSizes})}
+            >
+              <Monaco />
+              <Markdown />
+            </Split>
+            <div className="split-panel-snapping-dots" />
+          </section>
+        </article>
+      </main>
+      <style>{dotSnapCss}</style>
+    </>
   );
 }
 
